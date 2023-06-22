5b211ed691507415946445a86acf7a126ed02556
木村愛哉 <kimuramanaya@kimulocal.local>
2023-06-22 22:31:07 +0900

    リファクタリング

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,34 +9,36 @@ class SessionsController < ApplicationController
         # トークンのエンコード
         data = { user_id: user.id }
         access_token = JWT.encode({ data: data, exp: Time.current.since(30.seconds).to_i }, 'secret')
-        refresh_token = JWT.encode({ data: data, exp: Time.current.since(1.day).to_i }, 'refresh_secret')
+        # Redisによるセッション管理を実装したいので一旦使用しない
+        # refresh_token = JWT.encode({ data: data, exp: Time.current.since(1.day).to_i }, 'refresh_secret')
   
         # リフレッシュトークン生成
-        user.update(refresh_token: refresh_token)
+        # user.update(refresh_token: refresh_token)
   
-        # ここでブラウザにクッキー保存
-        cookies[:jwt] = refresh_token
+        # ここでブラウザにクッキー保存  ※クッキーは一旦使用しない
+        # cookies[:jwt] = refresh_token
+
         # アクセストークン送信
         render json: { accessToken: access_token, id: user.id }, status: :ok
       else
         render status: :unauthorized
       end
       
-      # リフレッシュする際の処理
-      def refresh
-        return render status: :unauthorized unless cookies[:jwt]
+      # リフレッシュする際の処理 ※Redisによるセッション管理を行うので一旦なし
+      # def refresh
+      #   return render status: :unauthorized unless cookies[:jwt]
     
-        refresh_token = cookies[:jwt]
-        user = User.find_by(refresh_token: refresh_token)
-        return render status: :forbidden unless user
+      #   refresh_token = cookies[:jwt]
+      #   user = User.find_by(refresh_token: refresh_token)
+      #   return render status: :forbidden unless user
     
-        payload, = JWT.decode(refresh_token, 'refresh_secret')
-        return render status: :forbidden unless payload['data']['user_id'] == user.id
+      #   payload, = JWT.decode(refresh_token, 'refresh_secret')
+      #   return render status: :forbidden unless payload['data']['user_id'] == user.id
     
-        data = { user_id: user.id }
-        access_token = JWT.encode({ data: data, exp: Time.current.since(30.seconds).to_i }, 'secret')
+      #   data = { user_id: user.id }
+      #   access_token = JWT.encode({ data: data, exp: Time.current.since(30.seconds).to_i }, 'secret')
     
-        render json: { accessToken: access_token } , status: :ok
-      end
+      #   render json: { accessToken: access_token } , status: :ok
+      # end
     end
   end
